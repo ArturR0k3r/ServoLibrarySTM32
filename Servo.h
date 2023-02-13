95caab69ab76d1e808833d8f6a92a42c09cae61d
ArturR0k3r <artur2003s.ass@gmail.com>
2023-02-13 13:56:54 +0200

    v2

--- Servo.h
+++ Servo.h
@@ -1,49 +1,47 @@
-#include <cstdint>
+/*
+ * File: SERVO.h
+ * Driver Name: [[ SERVO Motor ]]
+ * SW Layer:   ECUAL
+ * Created on: Jun 28, 2020
+ * Author:     Khaled Magdy
+ * -------------------------------------------
+ * For More Information, Tutorials, etc.
+ * Visit Website: www.DeepBlueMbedded.com
+ *
+ */
 
-#ifndef SERVO_H
-#define SERVO_H
-// Choose a microcontroller family
-//#define STM32F0
-//#define STM32F1
-//#define STM32F4
-//#define STM32L0
-//#define STM32L1
-//#define STM32L4
-//#define STM32F3
-//#define STM32H7
-//#define STM32F7
-//#define STM32G0
-#if defined(STM32F0)
-#include "stm32f0xx_hal.h"
-#elif defined(STM32F1)
+#ifndef SERVO_H_
+#define SERVO_H_
+
+#define HAL_TIM_MODULE_ENABLED
+
 #include "stm32f1xx_hal.h"
-#elif defined(STM32F4)
-#include "stm32f4xx_hal.h"
-#include "stm32f4xx_hal_gpio.h"
-#elif defined(STM32L0)
-#include "stm32l0xx_hal.h"
-#elif defined(STM32L1)
-#include "stm32l1xx_hal.h"
-#elif defined(STM32L4)
-#include "stm32l4xx_hal.h"
-#elif defined(STM32F3)
-#include "stm32f3xx_hal.h"
-#elif defined(STM32H7)
-#include "stm32h7xx_hal.h"
-#elif defined(STM32F7)
-#include "stm32f7xx_hal.h"
-#elif defined(STM32G0)
-#include "stm32g0xx_hal.h"
-#elif defined(STM32G4)
-#include "stm32g4xx_hal.h"
-#endif
-typedef struct Servo
-{
-	TIM_HandleTypeDef htimx;
-	uint16_t TIM_CHANNEL_x;
-	uint16_t duty;
-};
-void Servo_Init(Servo servo, TIM_HandleTypeDef htim, uint16_t TIM_CHANNEL, uint16_t dduty);
-void Servo_SetAngle(Servo servo, uint16_t dduty);
 
-#endif SERVO_H
+
+// The Number OF Servo Motors To Be Used In The Project
+#define SERVO_NUM  1
+
+typedef struct
+{
+	GPIO_TypeDef * SERVO_GPIO;
+	uint16_t       SERVO_PIN;
+	TIM_TypeDef*   TIM_Instance;
+	uint32_t*      TIM_CCRx;
+	uint32_t       PWM_TIM_CH;
+	uint32_t       TIM_CLK;
+	float          MinPulse;
+	float          MaxPulse;
+}SERVO_CfgType;
+
+
+/*-----[ Prototypes For All Functions ]-----*/
+
+void SERVO_Init(uint16_t au16_SERVO_Instance);
+void SERVO_MoveTo(uint16_t au16_SERVO_Instance, float af_Angle);
+void SERVO_RawMove(uint16_t au16_SERVO_Instance, uint16_t au16_Pulse);
+uint16_t SERVO_Get_MaxPulse(uint16_t au16_SERVO_Instance);
+uint16_t SERVO_Get_MinPulse(uint16_t au16_SERVO_Instance);
+void SERVO_Sweep(uint16_t au16_SERVO_Instance);
+
+
+#endif /* SERVO_H_ */
